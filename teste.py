2f7ed1bfacb5d6eae6ae846155654b6b4d1476cb
Vanessa Limana <vanessa4562008@hotmail.com>
2020-11-20 15:15:35 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,1 +1,9 @@
 print('olá mundo!')
+
+n = input('Digite um valor:')
+print('É alfanumerico?', n.isalnum())
+print('É maiusculo? ',n.isupper())
+print('É minusculo',n.islower())
+print('É capitalizado', n.istitle())
+print('São espaços', n.isspace())
+
