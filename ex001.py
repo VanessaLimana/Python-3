c34405c821d1b62e0dbd4eebe422766a73fd97ed
Vanessa Limana <vanessa4562008@hotmail.com>
2020-11-20 21:35:27 -0300

    Update ex001.py

--- ex001.py
+++ ex001.py
@@ -10,4 +10,4 @@ print('Salário com aumento {} = '.format(n1+n3))
 n=int(input('digite um número:'))
 a = n - 1
 s = n + 1
-print('analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(n,a,s))
+print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(n,a,s))
